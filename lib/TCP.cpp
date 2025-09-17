8086d6ec597e435f227d29412183821c593306a0
AlexandreFournier1 <alexandre.fournier@student.hepl.be>
2025-09-17 11:36:49 +0200

    Début de la librairie

---
+++ lib/TCP.cpp
@@ -0,0 +1,27 @@
+#include "TCP.h"
+
+int ServerSocket(int port)
+{
+
+}
+
+
+int Accept(int sEcoute,char *ipClient)
+{
+
+}
+
+int ClientSocket(char* ipServeur,int portServeur)
+{
+
+}
+
+int Send(int sSocket,char* data,int taille)
+{
+
+}
+
+int Receive(int sSocket,char* data)
+{
+	
+}
