e3404f2c1672e61a85c2756c6428c9b3eb616790
NoahDVKZ <noah.dellavalle@student.hepl.be>
2025-10-23 13:21:35 +0200

    Organisation DAO :art:

---
+++ Medecin_Java/Consultation/src/main/java/model/dao/ConsultationDAO.java
@@ -0,0 +1,9 @@
+package model.dao;
+
+import java.sql.*;
+import java.util.*;
+import java.util.logging.*;
+import java.time.LocalDate;
+public class ConsultationDAO {
+    private ConnectDB connectDB;
+}
---
+++ Medecin_Java/Consultation/src/main/java/model/viewmodel/ConsultationSearchVM.java
@@ -0,0 +1,4 @@
+package model.viewmodel;
+
+public class ConsultationSearchVM {
+}
