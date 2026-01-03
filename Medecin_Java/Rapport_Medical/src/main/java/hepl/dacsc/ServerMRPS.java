ac551954d152c466f44a5d78ebdd991b8e77b633
NoahDVKZ <noah.dellavalle@student.hepl.be>
2026-01-03 14:12:26 +0100

    correction merge

--- Medecin_Java/Rapport_Medical/src/main/java/hepl/dacsc/ServerMRPS.java
+++ Medecin_Java/Rapport_Medical/src/main/java/hepl/dacsc/ServerMRPS.java
@@ -5,17 +5,15 @@ import hepl.dacsc.ServerGeneriqueTCP.interfaces.Protocol;
 import hepl.dacsc.lib.MRPS;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.Security;
+import java.util.Properties;
 
 public class ServerMRPS {
 
-    // TODO : lire depuis un fichier properties
-    public static final int PORT_REPORT_SECURE = 60000;
-
     public static void main(String[] args) throws IOException {
         try {
-            // Initialisation Bouncy Castle
             Security.addProvider(new BouncyCastleProvider());
 
             Protocol protocol = new MRPS();
@@ -23,7 +21,12 @@ public class ServerMRPS {
             Logger logger = message ->
                     System.out.println("[SERVER MRPS] " + message);
 
-            ThreadServeurDemande server = new ThreadServeurDemande(PORT_REPORT_SECURE, protocol, logger);
+            Properties props = new Properties();
+            props.load(new FileInputStream("src/main/java/hepl/dacsc/config.properties"));
+
+            int port = Integer.parseInt(props.getProperty("PORT_REPORT_SECURE"));
+
+            ThreadServeurDemande server = new ThreadServeurDemande(port, protocol, logger);
 
             server.start();
 
