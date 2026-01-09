bf57950e1a40ff487f2ceb6450bfba3c2baf8e8e
alexlefou <alexlefou.progra@gmail.com>
2026-01-09 21:03:06 +0100

    Suite DAO toujours pas fini

--- Partie_Web/patient-app/src/model/dao/PatientDAO_API.ts
+++ Partie_Web/patient-app/src/model/dao/PatientDAO_API.ts
@@ -22,19 +22,64 @@ export class PatientDAO_API implements PatientAccessLayer {
 
     public async load(patientVM?: PatientVM): Promise<Array<Patient>> {
         this.selectedPatient = [];
+        let url = this.API_ENDPOINT;
         if (patientVM) {
-            const params = new URLSearchParams(); 
+            const params = new URLSearchParams();
+
             if (patientVM.id) {
-                params.append('id', patientVM.id.toString)
+                params.append('id', patientVM.id);
             }
+
+            if (patientVM.last_name) {
+                params.append('last_name', patientVM.last_name);
+            }
+
+            if (patientVM.first_name) {
+                params.append('first_name', patientVM.first_name);
+            }
+
+            if (patientVM.birth_date) {
+                params.append('birth_date', patientVM.birth_date);
+            }
+
+            url += `?${params.toString()}`;
         }
-        
+
+        const res = await fetch(url);
+
+        if (res.ok) {
+            this.selectedPatient = await res.json();
+        } else {
+            throw new PatientNotFoundError("Erreur lors du chargement des patients");
+        }
 
         return this.selectedPatient;
     }
 
     public async save(patient: Patient): Promise<void> {
-        
+        if (patient.id == null) {
+            const newPatient: Patient = {
+                id: patient.id,
+                last_name: patient.last_name,
+                first_name: patient.first_name,
+                birth_date: patient.birth_date
+            }
+
+            const res = await fetch(this.API_ENDPOINT, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newPatient)
+            })
+
+            if (!res.ok) {
+                throw new PatientNotFoundError("Erreur lors de l'ajout du patient")
+            }
+
+            const addedPatient = await res.json()
+            console.log("Patient ajouté :", addedPatient)
+        } 
     }
 
     public async delete(item: string | Patient): Promise<void> {
